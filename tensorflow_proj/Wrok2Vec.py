6f973ef160013ccfa88007a4f9596307589f0055
XuanBin Qiu <xuanbin.qiu@ericsson.com>
2018-04-29 16:37:43 +0200

    add work2vec model

--- tensorflow_proj/Wrok2Vec.py
+++ tensorflow_proj/Wrok2Vec.py
@@ -1,187 +1,296 @@
+# Copyright 2015 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""Basic word2vec example."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
 import collections
 import math
 import os
+import sys
+import argparse
 import random
 import zipfile
+
 import numpy as np
-import urllib
+from six.moves import urllib
+from six.moves import xrange  # pylint: disable=redefined-builtin
 import tensorflow as tf
-import matplotlib.pyplot as plt
-from sklearn.manifold import TSNE
+
+from tensorflow.contrib.tensorboard.plugins import projector
+
+# Give a folder path as an argument with '--log_dir' to save
+# TensorBoard summaries. Default is a log folder in current directory.
+current_path = os.path.dirname(os.path.realpath(sys.argv[0]))
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    '--log_dir',
+    type=str,
+    default=os.path.join(current_path, 'log'),
+    help='The log directory for TensorBoard summaries.')
+FLAGS, unparsed = parser.parse_known_args()
+
+# Create the directory for TensorBoard variables if there is not.
+if not os.path.exists(FLAGS.log_dir):
+    os.makedirs(FLAGS.log_dir)
+
+data_index = 0
 
 
-def maybe_download(url, filename, expected_bytes):
-    if not os.path.exists(filename):
-        filename, _, _ = urllib.request.urlretrieve(url + filename, filename)
-
-    statinfo = os.stat(filename)
+def maybe_download(filename, expected_bytes):
+    """Download a file if not present, and make sure it's the right size."""
+    url = 'http://mattmahoney.net/dc/'
+    local_filename = os.path.join(filename)
+    if not os.path.exists(local_filename):
+        local_filename, _ = urllib.request.urlretrieve(url + filename, local_filename)
+    statinfo = os.stat(local_filename)
     if statinfo.st_size == expected_bytes:
-        print("Found and verified ", filename)
+        print('Found and verified', filename)
     else:
         print(statinfo.st_size)
-        raise Exception(
-            "Fail to verify " + filename
-        )
-    return filename
+        raise Exception('Failed to verify ' + local_filename +
+                        '. Can you get to it with a browser?')
+    return local_filename
 
 
+filename = maybe_download('text8.zip', 31344016)
+
+
+# Read the data into a list of strings.
 def read_data(filename):
+    """Extract the first file enclosed in a zip file as a list of words."""
     with zipfile.ZipFile(filename) as f:
         data = tf.compat.as_str(f.read(f.namelist()[0])).split()
     return data
 
 
-def build_data_set(words, vocabluary_size):
-
+def build_dataset(words, n_words):
+    """Process raw inputs into a dataset."""
+    # data - list of codes (integers from 0 to vocabulary_size-1).
+    # This is the original text but words are replaced by their codes
+    # count - map of words(strings) to count of occurrences
+    # dictionary - map of words(strings) to their codes(integers)
+    # reverse_dictionary - maps codes(integers) to words(strings)
     count = [['UNK', -1]]
-    count.extend(collections.Counter(words).most_common(vocabluary_size-1))
+    count.extend(collections.Counter(words).most_common(n_words - 1))
     dictionary = dict()
     for word, _ in count:
         dictionary[word] = len(dictionary)
     data = list()
     unk_count = 0
     for word in words:
-        if word in dictionary:
-            index = dictionary[word]
-        else:
-            index = 0
+        index = dictionary.get(word, 0)
+        if index == 0:  # dictionary['UNK']
             unk_count += 1
         data.append(index)
     count[0][1] = unk_count
-    reverse_dict = dict(zip(dictionary.values(), dictionary.keys()))
-    return data, count, dictionary, reverse_dict
+    reversed_dictionary = dict(zip(dictionary.values(), dictionary.keys()))
+    return data, count, dictionary, reversed_dictionary
 
 
 def generate_batch(data, batch_size, num_skips, skip_window):
-    """
-    split the data into batches for training purposes
-
-    batch_size: size of each batch
-    num_skips: amount of examples that each word will generate
-    skips_window: how far can a word be associate, 1 means the word will only assocaite to the word around it
-
-    """
-    data_index = 0
     global data_index
     assert batch_size % num_skips == 0
-    assert num_skips <= 2* skip_window
+    assert num_skips <= 2 * skip_window
     batch = np.ndarray(shape=(batch_size), dtype=np.int32)
     labels = np.ndarray(shape=(batch_size, 1), dtype=np.int32)
-    span = 2*skip_window + 1
-    buffer = collections.deque(maxlen=span)
-
-    for _ in range(span):
-        buffer.append(data[data_index])
-        data_index = (data_index +1) % len(data)
-
-    for i in range(batch_size// num_skips):
-        target = skip_window
-        targets_to_avoid = [skip_window]
-        for j in range(num_skips):
-            while target in targets_to_avoid:
-                target = random.randint(0, span-1)
-            batch[i * num_skips +j] = buffer[skip_window]
-            labels[i * num_skips+j, 0] = buffer[target]
-        buffer.append(data[data_index])
-        data_index = (data_index +1) % len(data)
+    span = 2 * skip_window + 1  # [ skip_window target skip_window ]
+    buffer = collections.deque(maxlen=span)  # pylint: disable=redefined-builtin
+    if data_index + span > len(data):
+        data_index = 0
+    buffer.extend(data[data_index:data_index + span])
+    data_index += span
+    for i in range(batch_size // num_skips):
+        context_words = [w for w in range(span) if w != skip_window]
+        words_to_use = random.sample(context_words, num_skips)
+        for j, context_word in enumerate(words_to_use):
+            batch[i * num_skips + j] = buffer[skip_window]
+            labels[i * num_skips + j, 0] = buffer[context_word]
+        if data_index == len(data):
+            buffer.extend(data[0:span])
+            data_index = span
+        else:
+            buffer.append(data[data_index])
+            data_index += 1
+    # Backtrack a little bit to avoid skipping words in the end of a batch
+    data_index = (data_index + len(data) - span) % len(data)
     return batch, labels
 
 
-def generate_parametres():
+def check_result(step, similarity, valid_size, valid_examples, reverse_dictionary, executed=False):
+    if executed:
+        if step % 10000 == 0:
+            sim = similarity.eval()
+            for i in xrange(valid_size):
+                valid_word = reverse_dictionary[valid_examples[i]]
+                top_k = 8  # number of nearest neighbors
+                nearest = (-sim[i, :]).argsort()[1:top_k + 1]
+                log_str = 'Nearest to %s:' % valid_word
+                for k in xrange(top_k):
+                    close_word = reverse_dictionary[nearest[k]]
+                    log_str = '%s %s,' % (log_str, close_word)
+                print(log_str)
+
+
+def prepare_init_parameter():
     batch_size = 128
-    embedding_size = 128
-    skip_window = 4
-    num_skips = 4
-    valid_size = 16
-    valid_window = 16
-    valid_examples = np.random.choice(valid_window, valid_size, replace=False)
-    num_sampled = 64
+    embedding_size = 128  # Dimension of the embedding vector.
+    skip_window = 1  # How many words to consider left and right.
+    num_skips = 2  # How many times to reuse an input to generate a label.
+    num_sampled = 64  # Number of negative examples to sample.
+    vocabulary_size = 50000
 
-
-def visulization_with_labels(low_dim_embs, labels, filename ='tsne.png'):
-    assert low_dim_embs.shape[0] >= len(labels)
-    plt.figure(figsize=(18, 18))
-    for i, label in enumerate(labels):
-        x, y = low_dim_embs[i,:]
-        plt.scatter(x, y)
-        plt.annotate(label, xy=(x, y), xytext=(5, 2), textcoords="offset points", ha="right",va="bottom")
-    plt.savefig(filename)
+    # We pick a random validation set to sample nearest neighbors. Here we limit the
+    # validation samples to the words that have a low numeric ID, which by
+    # construction are also the most frequent. These 3 variables are used only for
+    # displaying model accuracy, they don't affect calculation.
+    valid_size = 16  # Random set of words to evaluate similarity on.
+    valid_window = 100  # Only pick dev samples in the head of the distribution.
+    return batch_size, embedding_size, skip_window, num_skips, num_sampled, valid_size, valid_window, vocabulary_size
 
 
 def main():
-    url = "http://mattmahoney.net/dc/"
-    vocabluary_size = 50000
-    filename = maybe_download(url, 'text8.zip', 31344016)
-    words = read_data(filename)
-    data, count, dictionary, reversed_dict = build_data_set(words, vocabluary_size)
-
-
-    batch_size = 128
-    embedding_size = 128
-    skip_window = 1
-    num_skips = 2
-    valid_size = 16
-    valid_window = 16
+    batch_size, embedding_size, skip_window, num_skips, num_sampled, valid_size, valid_window, vocabulary_size = prepare_init_parameter()
     valid_examples = np.random.choice(valid_window, valid_size, replace=False)
-    num_sampled = 64
 
+    vocabulary = read_data(filename)
+
+    data, count, dictionary, reverse_dictionary = build_dataset(vocabulary, vocabulary_size)
+    del vocabulary  # Hint to reduce memory.
 
     graph = tf.Graph()
+
     with graph.as_default():
-        train_inputs = tf.placeholder(tf.int32, shape=[batch_size])
-        train_labels = tf.placeholder(tf.int32, shape=[batch_size, 1])
-        valid_dataset = tf.constant(valid_examples, dtype=tf.int32)
+        # Input data.
+        with tf.name_scope('inputs'):
+            train_inputs = tf.placeholder(tf.int32, shape=[batch_size])
+            train_labels = tf.placeholder(tf.int32, shape=[batch_size, 1])
+            valid_dataset = tf.constant(valid_examples, dtype=tf.int32)
 
+        # Ops and variables pinned to the CPU because of missing GPU implementation
         with tf.device('/cpu:0'):
-            """ make sure it runs on cpu, since some of the calculation is not done for GPU yet """
-            embeddings = tf.Variable(tf.random_uniform([vocabluary_size, embedding_size], -1.0, 1.0))
-            embed = tf.nn.embedding_lookup(embeddings, train_inputs)
-            nce_weights = tf.Variable(tf.truncated_normal([vocabluary_size, embedding_size], stddev=1.0 / math.sqrt(embedding_size)))
-            nce_bias = tf.Variable(tf.zeros([vocabluary_size]))
+            # Look up embeddings for inputs.
+            with tf.name_scope('embeddings'):
+                embeddings = tf.Variable(
+                    tf.random_uniform([vocabulary_size, embedding_size], -1.0, 1.0))
+                embed = tf.nn.embedding_lookup(embeddings, train_inputs)
 
-        loss_func = tf.reduce_mean(tf.nn.nce_loss(weights=nce_weights, biases=nce_bias,labels=train_labels, inputs=embed,
-                                                  num_sampled=num_sampled, num_classes=vocabluary_size))
-        optimizer = tf.train.GradientDescentOptimizer(1.0).minimize(loss_func)
+            # Construct the variables for the NCE loss
+            with tf.name_scope('weights'):
+                nce_weights = tf.Variable(
+                    tf.truncated_normal(
+                        [vocabulary_size, embedding_size],
+                        stddev=1.0 / math.sqrt(embedding_size)))
+            with tf.name_scope('biases'):
+                nce_biases = tf.Variable(tf.zeros([vocabulary_size]))
+
+        # Compute the average NCE loss for the batch
+        with tf.name_scope('loss'):
+            loss = tf.reduce_mean(
+                tf.nn.nce_loss(
+                    weights=nce_weights,
+                    biases=nce_biases,
+                    labels=train_labels,
+                    inputs=embed,
+                    num_sampled=num_sampled,
+                    num_classes=vocabulary_size))
+
+        # Add the loss value as a scalar to summary.
+        tf.summary.scalar('loss', loss)
+
+        # Construct the SGD optimizer using a learning rate of 1.0.
+        with tf.name_scope('optimizer'):
+            optimizer = tf.train.GradientDescentOptimizer(1.0).minimize(loss)
+
+        # Compute the cosine similarity between minibatch examples and all embeddings.
         norm = tf.sqrt(tf.reduce_sum(tf.square(embeddings), 1, keep_dims=True))
         normalized_embeddings = embeddings / norm
-        valid_embeddings = tf.nn.embedding_lookup(normalized_embeddings, valid_dataset)
+        valid_embeddings = tf.nn.embedding_lookup(normalized_embeddings,
+                                                  valid_dataset)
         similarity = tf.matmul(valid_embeddings, normalized_embeddings, transpose_b=True)
 
+        # Merge all summaries.
+        merged = tf.summary.merge_all()
+
+        # Add variable initializer.
         init = tf.global_variables_initializer()
-        num_steps = 100001
 
-        with tf.Session(graph=graph) as session:
-            init.run()
-            print("Initializing")
+        # Create a saver.
+        saver = tf.train.Saver()
 
-            average_loss = 0
-            for step in range(num_steps):
-                batch_inputs, batch_labels = generate_batch(data, batch_size, num_skips, skip_window)
-                _, loss_val = session.run([optimizer, loss_func], feed_dict={train_inputs: batch_inputs, train_labels: batch_labels})
-                average_loss += loss_val
-                if step % 2000 == 0:
-                    if step > 0:
-                        average_loss /= 2000
-                    print("Average loss at step ", step, " : ", average_loss)
-                    average_loss = 0
-                if step % 10000 == 0:
-                    sim = similarity.eval()
-                    for i in range(valid_size):
-                        valid_word = reversed_dict[valid_examples[i]]
-                        top_k = 8
-                        nearest = (-sim[i,:]).argsort()[1:top_k+1]
-                        log_str = "Nearest to %s : " % valid_word
-                        for k in range(top_k):
-                            close_word = reversed_dict[nearest[k]]
-                            log_str = "%s %s," % (log_str, close_word)
-                        print(log_str)
-            final_embeddings = normalized_embeddings.eval()
+    num_steps = 50001
 
-    tsne = TSNE(perplexity=30, n_components=2, init="pca", n_iter=5000)
-    plot_only = 100
-    low_dims_embs = tsne.fit_transform(final_embeddings[:plot_only, :])
-    labels = [reversed_dict[i] for i in range(plot_only)]
-    visulization_with_labels(low_dims_embs, labels)
+    with tf.Session(graph=graph) as session:
+        # Open a writer to write summaries.
+        writer = tf.summary.FileWriter(FLAGS.log_dir, session.graph)
+
+        # We must initialize all variables before we use them.
+        init.run()
+
+        average_loss = 0
+        for step in xrange(num_steps):
+            batch_inputs, batch_labels = generate_batch(data, batch_size, num_skips, skip_window)
+            feed_dict = {train_inputs: batch_inputs, train_labels: batch_labels}
+
+            # Define metadata variable.
+            run_metadata = tf.RunMetadata()
+
+            # We perform one update step by evaluating the optimizer op (including it
+            # in the list of returned values for session.run()
+            # Also, evaluate the merged op to get all summaries from the returned "summary" variable.
+            # Feed metadata variable to session for visualizing the graph in TensorBoard.
+            _, summary, loss_val = session.run(
+                [optimizer, merged, loss],
+                feed_dict=feed_dict,
+                run_metadata=run_metadata)
+            average_loss += loss_val
+
+            # Add returned summaries to writer in each step.
+            writer.add_summary(summary, step)
+            # Add metadata to visualize the graph for the last run.
+            if step == (num_steps - 1):
+                writer.add_run_metadata(run_metadata, 'step%d' % step)
+
+            if step % 2000 == 0 and step > 0:
+                average_loss /= 2000
+                # The average loss is an estimate of the loss over the last 2000 batches.
+                print('Average loss at step ', step, ': ', average_loss)
+                average_loss = 0
+
+            # Note that this is expensive (~20% slowdown if computed every 500 steps)
+            check_result(step, similarity, valid_size, valid_examples, reverse_dictionary, executed=False)
+
+        # Write corresponding labels for the embeddings.
+        with open(FLAGS.log_dir + '/metadata.tsv', 'w') as f:
+            for i in xrange(vocabulary_size):
+                f.write(reverse_dictionary[i] + '\n')
+
+        # Save the model for checkpoints.
+        saver.save(session, os.path.join(FLAGS.log_dir, 'model.ckpt'))
+
+        # Create a configuration for visualizing embeddings with the labels in TensorBoard.
+        config = projector.ProjectorConfig()
+        embedding_conf = config.embeddings.add()
+        embedding_conf.tensor_name = embeddings.name
+        embedding_conf.metadata_path = os.path.join(FLAGS.log_dir, 'metadata.tsv')
+        projector.visualize_embeddings(writer, config)
+
+    writer.close()
 
 
 if __name__ == '__main__':

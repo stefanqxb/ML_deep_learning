33150cbcbc422faef4508fe949a399fcdea4069f
XuanBin Qiu <xuanbin.qiu@ericsson.com>
2018-05-01 12:13:10 +0200

    modify work2vec

--- tensorflow_proj/Wrok2Vec.py
+++ tensorflow_proj/Wrok2Vec.py
@@ -33,8 +33,6 @@ import tensorflow as tf
 
 from tensorflow.contrib.tensorboard.plugins import projector
 
-# Give a folder path as an argument with '--log_dir' to save
-# TensorBoard summaries. Default is a log folder in current directory.
 current_path = os.path.dirname(os.path.realpath(sys.argv[0]))
 
 parser = argparse.ArgumentParser()
@@ -66,9 +64,6 @@ def maybe_download(filename, expected_bytes):
         raise Exception('Failed to verify ' + local_filename +
                         '. Can you get to it with a browser?')
     return local_filename
-
-
-filename = maybe_download('text8.zip', 31344016)
 
 
 # Read the data into a list of strings.
@@ -164,7 +159,29 @@ def prepare_init_parameter():
     return batch_size, embedding_size, skip_window, num_skips, num_sampled, valid_size, valid_window, vocabulary_size
 
 
+def generate_ops(train_inputs, vocabulary_size, embedding_size):
+    # Ops and variables pinned to the CPU because of missing GPU implementation
+    with tf.device('/cpu:0'):
+        # Look up embeddings for inputs.
+        with tf.name_scope('embeddings'):
+            embeddings = tf.Variable(
+                tf.random_uniform([vocabulary_size, embedding_size], -1.0, 1.0))
+            embed = tf.nn.embedding_lookup(embeddings, train_inputs)
+
+        # Construct the variables for the NCE loss
+        with tf.name_scope('weights'):
+            nce_weights = tf.Variable(
+                tf.truncated_normal(
+                    [vocabulary_size, embedding_size],
+                    stddev=1.0 / math.sqrt(embedding_size)))
+        with tf.name_scope('biases'):
+            nce_biases = tf.Variable(tf.zeros([vocabulary_size]))
+    return embeddings, embed, nce_weights, nce_biases
+
+
 def main():
+
+    filename = maybe_download('text8.zip', 31344016)
     batch_size, embedding_size, skip_window, num_skips, num_sampled, valid_size, valid_window, vocabulary_size = prepare_init_parameter()
     valid_examples = np.random.choice(valid_window, valid_size, replace=False)
 
@@ -176,28 +193,12 @@ def main():
     graph = tf.Graph()
 
     with graph.as_default():
-        # Input data.
         with tf.name_scope('inputs'):
             train_inputs = tf.placeholder(tf.int32, shape=[batch_size])
             train_labels = tf.placeholder(tf.int32, shape=[batch_size, 1])
             valid_dataset = tf.constant(valid_examples, dtype=tf.int32)
 
-        # Ops and variables pinned to the CPU because of missing GPU implementation
-        with tf.device('/cpu:0'):
-            # Look up embeddings for inputs.
-            with tf.name_scope('embeddings'):
-                embeddings = tf.Variable(
-                    tf.random_uniform([vocabulary_size, embedding_size], -1.0, 1.0))
-                embed = tf.nn.embedding_lookup(embeddings, train_inputs)
-
-            # Construct the variables for the NCE loss
-            with tf.name_scope('weights'):
-                nce_weights = tf.Variable(
-                    tf.truncated_normal(
-                        [vocabulary_size, embedding_size],
-                        stddev=1.0 / math.sqrt(embedding_size)))
-            with tf.name_scope('biases'):
-                nce_biases = tf.Variable(tf.zeros([vocabulary_size]))
+        embeddings, embed, nce_weights, nce_biases = generate_ops(train_inputs, vocabulary_size, embedding_size)
 
         # Compute the average NCE loss for the batch
         with tf.name_scope('loss'):
@@ -213,15 +214,14 @@ def main():
         # Add the loss value as a scalar to summary.
         tf.summary.scalar('loss', loss)
 
-        # Construct the SGD optimizer using a learning rate of 1.0.
         with tf.name_scope('optimizer'):
-            optimizer = tf.train.GradientDescentOptimizer(1.0).minimize(loss)
+            optimizer = tf.train.AdamOptimizer(learning_rate=0.05).minimize(loss)
+            #optimizer = tf.train.GradientDescentOptimizer(1.0).minimize(loss)
 
         # Compute the cosine similarity between minibatch examples and all embeddings.
         norm = tf.sqrt(tf.reduce_sum(tf.square(embeddings), 1, keep_dims=True))
         normalized_embeddings = embeddings / norm
-        valid_embeddings = tf.nn.embedding_lookup(normalized_embeddings,
-                                                  valid_dataset)
+        valid_embeddings = tf.nn.embedding_lookup(normalized_embeddings, valid_dataset)
         similarity = tf.matmul(valid_embeddings, normalized_embeddings, transpose_b=True)
 
         # Merge all summaries.
@@ -233,13 +233,11 @@ def main():
         # Create a saver.
         saver = tf.train.Saver()
 
-    num_steps = 50001
+    num_steps = 100001
 
     with tf.Session(graph=graph) as session:
         # Open a writer to write summaries.
         writer = tf.summary.FileWriter(FLAGS.log_dir, session.graph)
-
-        # We must initialize all variables before we use them.
         init.run()
 
         average_loss = 0
@@ -260,7 +258,6 @@ def main():
                 run_metadata=run_metadata)
             average_loss += loss_val
 
-            # Add returned summaries to writer in each step.
             writer.add_summary(summary, step)
             # Add metadata to visualize the graph for the last run.
             if step == (num_steps - 1):
@@ -268,7 +265,6 @@ def main():
 
             if step % 2000 == 0 and step > 0:
                 average_loss /= 2000
-                # The average loss is an estimate of the loss over the last 2000 batches.
                 print('Average loss at step ', step, ': ', average_loss)
                 average_loss = 0
 
